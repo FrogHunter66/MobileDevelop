5ae5191c8db380dfd4ee359317a587d39f0fea73
Nikita <nikita.fdorov99@mail.ru>
2024-02-24 13:32:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    Context context = MainActivity.this;
+
+    int duration = Toast.LENGTH_SHORT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+        Toast.makeText(context, "onCreate", duration).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        Toast.makeText(context, "onStart", duration).show();
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG, "onStop");
+        Toast.makeText(context, "onStop", duration).show();
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+        Toast.makeText(context, "onDestroy", duration).show();
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause");
+        Toast.makeText(context, "onPause", duration).show();
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume");
+        Toast.makeText(context, "onResume", duration).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart");
+        Toast.makeText(context, "onRestart", duration).show();
+
+    }
+
+}
